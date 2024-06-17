558cd1731f607a30b20fef1cb508cd8d03e73129
ali shefaee <alishefaee@gmail.com>
2024-06-17 21:16:43 +0330

    feat: init websocket

--- src/sockets/on-connection.ts
+++ src/sockets/on-connection.ts
@@ -9,9 +9,27 @@ export function onConnection(io: Server) {
         socket.on('s:meeting:create', onCreateMeeting(io, socket));
         socket.on('s:meeting:join', onJoinMeeting(io, socket));
         socket.on('s:msg:new', onMessage(io, socket));
-        socket.on('audioStream', (audioData) => {
-            console.log('audioStream');
-            socket.broadcast.emit('audioStream', audioData);
+        // socket.on('audioStream', (audioData) => {
+        //     console.log('audioStream');
+        //     socket.broadcast.emit('audioStream', audioData);
+        // });
+        socket.on('message', (message) => {
+            io.emit('message', message);
+        });
+
+        socket.on('offer', (offer) => {
+            console.log('Offer received:', offer);
+            socket.broadcast.emit('offer', offer);
+        });
+
+        socket.on('answer', (answer) => {
+            console.log('Answer received:', answer);
+            socket.broadcast.emit('answer', answer);
+        });
+
+        socket.on('candidate', (candidate) => {
+            console.log('Candidate received:', candidate);
+            socket.broadcast.emit('candidate', candidate);
         });
         socket.on('disconnect', () => {
             console.log('Client disconnected:', socket.id)
